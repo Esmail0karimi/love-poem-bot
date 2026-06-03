4654f4e6d2da8eb5705edc696b98cbfaaa8d66a4
Esmail <91189643+Esmail0karimi@users.noreply.github.com>
2026-06-03 11:19:41 +0330

    Update send-poem.js

--- send-poem.js
+++ send-poem.js
@@ -1,27 +1,102 @@
-console.log("BOT_TOKEN exists:", !!process.env.BOT_TOKEN);
-console.log("CHAT_ID exists:", !!process.env.CHAT_ID);
 const fs = require("fs");
+const zlib = require("zlib");
 
-const poems = JSON.parse(
-  fs.readFileSync("poems.json", "utf8")
+// خواندن فایل فشرده
+const compressed =
+fs.readFileSync("poems.json.gz");
+
+const poems =
+JSON.parse(
+zlib.gunzipSync(compressed)
 );
 
-const poem =
-  poems[Math.floor(Math.random() * poems.length)];
+// فایل وضعیت
+let state={usedPoems:[]};
 
-fetch(
-  `https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`,
-  {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      chat_id: process.env.CHAT_ID,
-      text: poem
-    })
-  }
+if(fs.existsSync("state.json")){
+    state=JSON.parse(
+        fs.readFileSync(
+            "state.json",
+            "utf8"
+        )
+    );
+}
+
+let available=
+poems.filter(
+(_,index)=>
+!state.usedPoems.includes(index)
+);
+
+// اگر همه استفاده شدند
+if(available.length===0){
+
+    state.usedPoems=[];
+
+    available=poems;
+}
+
+// انتخاب شعر تصادفی
+const poem=
+available[
+Math.floor(
+Math.random()*available.length
 )
-.then(res => res.json())
+];
+
+// ذخیره شناسه
+const poemIndex=
+poems.indexOf(poem);
+
+state.usedPoems.push(
+poemIndex
+);
+
+// ذخیره state
+fs.writeFileSync(
+"state.json",
+JSON.stringify(
+state,
+null,
+2
+)
+);
+
+// محدودیت تلگرام
+let message=poem;
+
+if(message.length>4096){
+
+message=
+message.substring(
+0,
+4000
+)+"...";
+}
+
+// ارسال
+fetch(
+`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`,
+{
+method:"POST",
+
+headers:{
+"Content-Type":"application/json"
+},
+
+body:JSON.stringify({
+
+chat_id:
+process.env.CHAT_ID,
+
+text:message
+
+})
+
+}
+)
+.then(
+res=>res.json()
+)
 .then(console.log)
 .catch(console.error);
